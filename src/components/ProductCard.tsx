9cc5a47d647987c5d0820a7b307c5322d4a5ceca
mohamed <mohamed.mostafa.ibrahim.2005@gmail.com>
2026-04-12 16:33:14 +0200

    Initializing

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ProductsType } from '@/types'
+
+const ProductCard = ({product}: {product: ProductsType}) => {
+  return (
+    <div>ProductCard</div>
+  )
+}
+
+export default ProductCard
